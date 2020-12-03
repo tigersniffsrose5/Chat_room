a17063f75b596ee4cfa3da681fe18e5853e9376c
crista <1228335053@qq.com>
2020-12-03 19:44:01 +0800

    add 1 file

---
+++ Client/dealapply.c
@@ -0,0 +1,93 @@
+#include "client.h"
+
+void dealapply()
+{
+    WINDOW *aboutWin;
+    int hang = 12;
+    int i, l, key;
+    char str[10][30];
+    char str_name[10][30];
+    Message_box *head = friend_request;
+
+    clear();
+    touchwin(stdscr);                    //激活stdrc窗口
+    wrefresh(stdscr);                    //将窗口显示出来
+    aboutWin = newwin(16, 40, 8, 48);
+    Wind(aboutWin, 0, 0, 15, 39);
+    touchwin(aboutWin);                
+    wrefresh(aboutWin);
+
+    noecho();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+
+    bSubOpen = 0;                        //没在主函数界面，切换标志位 
+
+    mvaddstr(10, 58, "****好友申请列表****");
+
+    i = 0;
+    while ( head != NULL ) {
+        strcpy(str[i], head->recv_name);
+        strcpy(str_name[i], head->recv_name);
+        strcat(str[i], "的申请信息");
+        mvaddstr(hang, 61, str[i]);
+        hang += 2;
+        i++;
+        head = head->next; 
+    }
+
+    strcpy(str[i], "*****返回*****");
+    mvaddstr(hang, 61, str[i]);
+    
+    attron(A_REVERSE);  
+    mvaddstr(12, 61, str[0]);  
+    attroff(A_REVERSE);
+
+    l = 0;
+    while ( 1 ) {
+
+        mvprintw(12, 87, "|");
+        key = getch();
+
+        if ( key == KEY_UP || key == KEY_DOWN ) {
+
+            mvaddstr(12+2*l, 61, str[l]);
+            
+            if ( key == KEY_UP ) {
+                l = l == 0 ? l+i : l-1; 
+            }
+
+            else {
+                l = l == i ? 0 : l+1;
+            }
+
+            attron(A_REVERSE);  
+            mvaddstr(12+2*l, 61, str[l]);  
+            attroff(A_REVERSE);
+
+        }
+
+        else if ( key == ENTER ) {
+            break;    
+        }
+    }
+
+    if ( l != i ) {
+        
+        head = friend_request;
+
+        while ( head != NULL ) {
+            if ( strcmp(head->recv_name, str_name[l]) == 0 ) {
+                add_friend(head->message);
+            }
+            head = head->next;
+        }
+
+        delet(&friend_request, str_name[l]);
+
+    }
+
+    clear();
+    functionmenu();
+
+}
---
+++ Client/list.c
@@ -0,0 +1,87 @@
+#include "client.h"
+
+void add(Message_box **Head, const char *message_t)
+{
+
+    cJSON *root = cJSON_Parse(message_t);
+    cJSON *item = cJSON_GetObjectItem(root, "friend_name");
+    
+    Message_box *p = (Message_box *)malloc(sizeof(Message_box));
+    Message_box *last = *Head;
+
+    strcpy(p->recv_name, item->valuestring);
+    strcpy(p->message, message_t);
+    p->next = NULL;
+
+    if ( last ) {
+        while ( last->next ) {
+            last = last->next;
+        }
+        last->next = p;
+    }
+
+    else {
+        *Head = p;
+    }
+}
+
+void delet(Message_box **Head, const char *name)
+{
+    Message_box *p = *Head, *q;
+
+    for ( q = NULL; p; q = p, p = p->next ) {
+        if ( strcmp(p->recv_name, name) == 0 ) {
+            if ( q ) {
+                q->next = p->next;
+            }
+            else {
+                *Head = p->next;
+            }
+            free(p);
+            break;
+        }
+    }
+}   
+
+//void delet1(Message_box **Head, int fd_t)
+//{
+//    Message_box *p = *Head, *q;
+//
+//    for ( q = NULL ; p ; q = p, p = p->next ) {
+//        if ( p->fd == fd_t ) {
+//            if ( q ) {
+//                q->next = p->next;
+//            }
+//            else {
+//                *Head = p->next;
+//            }
+//            free(p);
+//            break;
+//        }
+//    }
+//}
+//
+//int search(Message_box *Head, const char *message_t)
+//{
+//    Message_box *p = Head, *q;
+//
+//    for ( q = NULL; p; q = p, p = p->next ) {
+//        if ( strcmp(p->messagee, message) == 0 ) {
+//            return p->fd;
+//        }
+//    }
+//
+//    return 0;
+//}
+//
+//
+//void fre(Message_box *Head)
+//{
+//    Message_box *p = Head, *q;
+//
+//    while ( p ) {
+//        q = p;
+//        p = p->next;
+//        free(q);
+//    }
+//}
